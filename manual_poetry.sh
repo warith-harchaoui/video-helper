dfb590186043bee3f2ddea9689ce0297b9458438
Warith Harchaoui <warith.harchaoui@jellysmack.com>
2024-10-28 11:26:28 +0100

    remove opencv

--- manual_poetry.sh
+++ manual_poetry.sh
@@ -13,7 +13,7 @@ PROJECT_NAME="video-helper"
 PYTHON_VERSION="3.12"
 ENV="env4vh"
 
-DEPENDENCIES="scikit-image ffmpeg-python opencv-python numpy vidgear git+https://github.com/warith-harchaoui/os-helper.git@main"
+DEPENDENCIES="scikit-image ffmpeg-python numpy vidgear git+https://github.com/warith-harchaoui/os-helper.git@main"
 DESCRIPTION="Video Helper is a Python library that provides utility functions for processing video files. It includes features like loading, converting, extracting frames as well as working with subtitle formats."
 AUTHORS="Warith Harchaoui <warith.harchaoui@gmail.com>, Mohamed Chelali <mohamed.t.chelali@gmail.com>, Bachir Zerroug <bzerroug@gmail.com>"
 
@@ -24,6 +24,7 @@ source ~/.zshrc
 if conda info --envs | grep -q "^$ENV"; then
     echo "Environment $ENV already exists, removing it..."
     conda deactivate
+    conda deactivate
     conda remove --name $ENV --all -y
 fi
 
@@ -31,15 +32,12 @@ fi
 echo "Creating environment $ENV..."
 conda create -y -n $ENV python=$PYTHON_VERSION
 conda activate $ENV
+conda install -y pip
+
 
 # Poetry setup
-pip install --upgrade poetry poetry2setup
+pip install --upgrade poetry poetry2setup setuptools
 
-# Remove previous Poetry files if they exist
-rm -f pyproject.toml poetry.lock
-
-# Initialize the Poetry project with required details
-poetry init --name $PROJECT_NAME --description "$DESCRIPTION" --author "$AUTHORS" --python "^$PYTHON_VERSION" -n
 
 # Convert the dependencies string into an array (compatible with zsh/bash)
 DEP_ARRAY=(${=DEPENDENCIES})
@@ -47,18 +45,37 @@ DEP_ARRAY=(${=DEPENDENCIES})
 # Loop through each dependency and add it with poetry
 for dep in "${DEP_ARRAY[@]}"; do
     echo "Adding $dep..."
-    poetry add "$dep"
+    pip install "$dep"
 done
 
-# Lock and install dependencies
+pip uninstall jaraco.classes
+pip freeze > requirements.txt
+
+# replace git commit hash with @main
+sed -i '' 's/@[a-f0-9]\{7,40\}/@main/g' requirements.txt
+
+rm -f pyproject.toml poetry.lock
+
+poetry init --name $PROJECT_NAME --description "$DESCRIPTION" --author "$AUTHORS" --python "^$PYTHON_VERSION" -n
+
+python requirements_to_toml.py \
+    --project_name "$PROJECT_NAME" \
+    --description "$DESCRIPTION" \
+    --authors "$AUTHORS" \
+    --python_version "^$PYTHON_VERSION" \
+    --requirements_file "requirements.txt" \
+    --output_file "pyproject.toml"
+
+# Initialize the Poetry project with required details
 poetry install
+
+
 
 # Generate setup.py and export requirements.txt
 poetry2setup > setup.py
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 
-# replace git commit hash with @main
-sed -i '' 's/@[a-f0-9]\{7,40\}/@main/g' requirements.txt
+
 
 
 # Create environment.yml for conda users (optional)
